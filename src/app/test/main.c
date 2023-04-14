6950db095dd8279022523c5aaf164080501c7214
JWWH <3181024576@qq.com>
2023-04-14 23:44:54 +0800

    debug信息输出

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -13,6 +13,7 @@
 #include "sys_plat.h"
 
 #include "netif_pcap.h"
+#include "debug.h"
 
 net_err_t netdev_init(void){
     netif_pcap_open();
@@ -20,12 +21,19 @@ net_err_t netdev_init(void){
     return NET_ERR_OK;
 }
 
+#define DBG_TEST    DBG_LEVEL_INFO
 int main(int argc, char const *argv[]){
+    dbg_info(DBG_TEST, "info");
+    dbg_error(DBG_TEST, "error");
+    dbg_waring(DBG_TEST, "warning");
+    dbg_assert(1==1, "failed");
+    dbg_assert(1==0, "failed");
+
     net_init();
     net_start();
 
     netdev_init();
-    
+
     while(1){
         sys_sleep(100);
     }

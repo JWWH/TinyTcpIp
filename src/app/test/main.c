db8157fef12f859899a1a46f27db2983f37d2285
JWWH <3181024576@qq.com>
2023-04-14 17:04:52 +0800

    操作系统接口

---
+++ src/app/test/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @author JWWH
+ * @brief 测试主程序
+ * @version 0.1
+ * @date 2023-04-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+    printf("Hello World!\n");
+    return 0;
+}
